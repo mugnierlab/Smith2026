c570cc17270708804687a771f1bf679db47bcd2d
JaclynSmith <60448949+JaclynSmith@users.noreply.github.com>
2026-02-18 23:15:36 -0500

    Update figure2A_2B_ED3.R
    add graph for ED3A

--- Figures/nature_figure2/figure2A_2B_ED3.R
+++ Figures/nature_figure2/figure2A_2B_ED3.R
@@ -21,7 +21,7 @@ all_E9 <- all_E9 %>%
   filter(type_of_event %in% c("target_to_donor", "donor_to_target", "target_to_donor_edge", "donor_to_target_edge")) %>%
   filter(day != "DMSO")
 
-# summary by guide position
+# summary by guide position, Extended Data Figure 3B & main figure 2A
 all_events_graph_E9 <- graph_all_events(all_E9, color_var = "donor_VSG", wrap_by = "genotype", cut_line = TRUE,
                                              cut_line_names = c("NoGuide", "Antat.141", "Antat.300R", "Antat.592", "Antat.792",
                                                                 "Antat.909R", "Antat.1357"), cut_line_locations = c(0, 242, 369, 693, 893, 979, 1458),
@@ -47,6 +47,15 @@ all_events_graph_E9_summary
 
 all_D3_reseq <- all_D3_reseq %>%
   filter(type_of_event %in% c("target_to_donor", "donor_to_target", "target_to_donor_edge", "donor_to_target_edge"))
+
+# summary by guide position, Extended Data Figure 3A
+all_events_graph_D3 <- graph_all_events(all_D3_reseq, color_var = "donor_VSG", wrap_by = "genotype", cut_line = TRUE,
+                                        cut_line_names = c("NoGuide", "Antat.141", "Antat.300R", "Antat.592", "Antat.792",
+                                                           "Antat.909R", "Antat.1357"), cut_line_locations = c(0, 242, 369, 693, 893, 979, 1458),
+                                        cut_line_colors = c("black"), leg = "Donor VSG", adjust_xaxis = TRUE, multiple = FALSE, vert_num = 7, log_scale = FALSE) # +
+  ggplot2::geom_vline(aes(xintercept = c(86))) +
+  ggplot2::geom_vline(aes(xintercept = c(1598))) +
+  ggplot2::geom_vline(aes(xintercept = c(1079)))
 
 # summary of all recomb sites
 all_events_graph_D3_2 <- graph_all_events(all_D3_reseq, color_var = "donor_VSG", wrap_by = "genotype", cut_line = TRUE,
